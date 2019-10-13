4c62c4163da43bafc6e34161889141c29d33369d
Nicole Clayton <50276082+nclayt0n@users.noreply.github.com>
2019-10-13 14:14:37 -0400

    added basic auth

--- src/components/LoginForm/LoginForm.js
+++ src/components/LoginForm/LoginForm.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import { Button, Input } from '../Utils/Utils'
-
+import TokenService from '../../services/token-service'
 export default class LoginForm extends Component {
   static defaultProps = {
     onLoginSuccess: () => {}
@@ -13,8 +13,8 @@ export default class LoginForm extends Component {
     const { user_name, password } = ev.target
 
     console.log('login form submitted')
-    console.log({ user_name, password })
-
+    TokenService.saveAuthToken(TokenService.makeBasicAuthToken(user_name.value,password.value))
+  
     user_name.value = ''
     password.value = ''
     this.props.onLoginSuccess()
